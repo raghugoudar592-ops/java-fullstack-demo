7ce6a025bc123a83680cc8ecb3d59fb4d1e177c2
Raghavendra Goudar <raghugoudar592@gmail.com>
2025-09-16 23:07:17 +0530

    Operators.java

--- Operators_lyst1726604254867.java
+++ Operators_lyst1726604254867.java
@@ -1,31 +1,4 @@
-/*
-* -----------------------------------------------------------------------------
-* 
-* Copyright <2024> <algorithms365>
-* 
-* Professional Coding Skills Workshops
-* 
-* Licensed under the MIT License:
-*  
-* https://opensource.org/licenses/MIT
-* 
-* For more information about algorithms365:
-* Visit Our Skills Website: https://skills.algorithms365.com/
-* Our Company Website: https://algorithms365.com/
-*
-* For Regular Updates Follow & Subscribe Us on Our Social Media Platforms:
-* Instagram: https://www.instagram.com/algorithms365/
-* YouTube: https://www.youtube.com/@algorithms365
-* Facebook: https://www.facebook.com/algorithms365 
-* Twitter(X): https://x.com/algorithms365
-* LinkedIn: https://www.linkedin.com/company/algorithms365-technologies-llp/
-* 
-* Join Our Communities:
-* WhatsApp: https://chat.whatsapp.com/K1K7wDMEXG0DJhqMCxFtht
-* Telegram: https://t.me/+hyVHXek9WM0zNWQ1
-* 
-* -----------------------------------------------------------------------------
-*/
+
 public class Operators {
 
     public static void main(String[] args) {
@@ -98,3 +71,4 @@ public class Operators {
         System.out.println("Instance check: 5 instanceof Integer -> " + (Integer.valueOf(5) instanceof Integer));
     }
 }
+
