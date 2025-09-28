53a6755e930d03c77c2aed3d57b3847cf1535f59
Raghavendra Goudar <raghugoudar592@gmail.com>
2025-09-28 22:20:11 +0530

    Add files via upload

---
+++ IsEven.java
@@ -0,0 +1,23 @@
+public class IsEven {
+    static boolean IsEven(int number){
+    if( n % 2 == 0)
+    {
+        return true;
+        System.out.println("The number is even");
+    }
+        else
+    {
+        return false;
+        System.out.println("The number is odd");
+    }
+ static void invoke_IsEven()
+ {
+    int number(10)
+    int number(11)
+ }   
+public static void main()
+{
+  invoke_IsEven()  
+}
+}
+}
---
+++ UniqueElements.java
@@ -0,0 +1,21 @@
+public class UniqueElements {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 3, 4, 4, 5};
+
+        System.out.print("Unique elements: ");
+        for (int i = 0; i < arr.length; i++) {
+            boolean isUnique = true;
+
+            for (int j = 0; j < arr.length; j++) {
+                if (i != j && arr[i] == arr[j]) {
+                    isUnique = false;
+                    break;
+                }
+            }
+
+            if (isUnique) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
+}
